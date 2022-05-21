19e959f09a897ed06125db3867ad5a0ed8cb5605
Youngmin Lee <66107284+2theb@users.noreply.github.com>
2022-05-21 11:12:00 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,433 @@
+var CONFIG = {
+ "data": {
+  "_lastModified": 1653098625055,
+  "ddfPath": "./data/시군_총인구_노인인구비율_1992-2021.csv",
+  "hasNameColumn": true,
+  "lastModified": 1653098625055,
+  "nameColumnPosition": 0,
+  "noCache": true,
+  "path": "./data/시군_총인구_노인인구비율_1992-2021.csv",
+  "reader": "ext-csv",
+  "sheet": "",
+  "timeInColumns": true
+ },
+ "locale": {
+  "filePath": "assets/translation/",
+  "id": "en"
+ },
+ "state": {
+  "entities": {
+   "autoconfig": {
+    "excludeIDs": [
+     "tag"
+    ],
+    "type": "entity_domain"
+   },
+   "dim": "시군구",
+   "filter": {},
+   "show": {},
+   "showFallback": {},
+   "showItemsMaxCount": null,
+   "skipFilter": false
+  },
+  "entities_colorlegend": {
+   "autoconfig": {
+    "excludeIDs": [
+     "tag"
+    ],
+    "type": "entity_domain"
+   },
+   "dim": "시군구",
+   "filter": {},
+   "show": {},
+   "showFallback": {},
+   "showItemsMaxCount": null,
+   "skipFilter": false
+  },
+  "marker": {
+   "allowSelectMultiple": true,
+   "axis_x": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log",
+      "genericLog",
+      "time",
+      "pow"
+     ]
+    },
+    "autoconfig": {
+     "index": 0,
+     "type": "measure"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "fixBaseline": null,
+    "scaleType": "linear",
+    "syncModels": [],
+    "use": "indicator",
+    "which": "총인구",
+    "zoomedMax": 11341381.1,
+    "zoomedMin": -405650.4
+   },
+   "axis_y": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log",
+      "genericLog",
+      "time",
+      "pow"
+     ]
+    },
+    "autoconfig": {
+     "index": 1,
+     "type": "measure"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "fixBaseline": null,
+    "scaleType": "linear",
+    "syncModels": [],
+    "use": "indicator",
+    "which": "노인인구비율",
+    "zoomedMax": 22.41,
+    "zoomedMin": 2.45
+   },
+   "color": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log",
+      "genericLog",
+      "time",
+      "pow",
+      "ordinal"
+     ]
+    },
+    "autoconfig": {},
+    "data": "data",
+    "palette": {
+     "0": "#4cd843",
+     "1": "#e83739",
+     "2": "#ff7f00",
+     "3": "#c027d4",
+     "4": "#d66425",
+     "5": "#0ab8d8",
+     "6": "#bcfa83",
+     "7": "#ff8684",
+     "8": "#ffb04b",
+     "9": "#f599f5",
+     "10": "#f4f459",
+     "11": "#7fb5ed",
+     "_default": "#ffb600"
+    },
+    "paletteHiddenKeys": [],
+    "paletteLabels": null,
+    "scaleType": "ordinal",
+    "spaceRef": null,
+    "syncModels": [
+     "marker_colorlegend"
+    ],
+    "use": "indicator",
+    "which": "시도"
+   },
+   "highlight": [],
+   "label": {
+    "autoconfig": {
+     "includeOnlyIDs": [
+      "name"
+     ],
+     "type": "string"
+    },
+    "data": "data",
+    "scaleType": "ordinal",
+    "syncModels": [],
+    "use": "property",
+    "which": "시군구"
+   },
+   "limit": 5000,
+   "opacityHighlightDim": 0.1,
+   "opacityRegular": 1,
+   "opacitySelectDim": 0.15,
+   "select": [
+    {
+     "labelOffset": [
+      0.042,
+      -0.104
+     ],
+     "trailStartTime": "1992",
+     "시군구": "서울"
+    },
+    {
+     "labelOffset": [
+      0.121,
+      0.05
+     ],
+     "trailStartTime": "1992",
+     "시군구": "부산(기장군 제외)"
+    },
+    {
+     "labelOffset": [
+      0.104,
+      0.048
+     ],
+     "trailStartTime": "1992",
+     "시군구": "대구(달성군 제외)"
+    },
+    {
+     "labelOffset": [
+      0.041,
+      0.062
+     ],
+     "trailStartTime": "1995",
+     "시군구": "광주"
+    },
+    {
+     "labelOffset": [
+      -0.01,
+      0.048
+     ],
+     "trailStartTime": "1997",
+     "시군구": "울산(울주군 제외)"
+    },
+    {
+     "labelOffset": [
+      0.054,
+      0.093
+     ],
+     "trailStartTime": "1992",
+     "시군구": "인천(강화군, 옹진군 제외)"
+    },
+    {
+     "labelOffset": [
+      0.059,
+      -0.054
+     ],
+     "trailStartTime": "2012",
+     "시군구": "세종"
+    },
+    {
+     "labelOffset": [
+      -0.05,
+      0
+     ],
+     "trailStartTime": "1995",
+     "시군구": "대전"
+    }
+   ],
+   "size": {
+    "allow": {
+     "scales": [
+      "ordinal",
+      "linear",
+      "log",
+      "genericLog",
+      "pow"
+     ]
+    },
+    "autoconfig": {
+     "index": 2,
+     "type": "measure"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "extent": [
+     0,
+     0.64
+    ],
+    "fixBaseline": 0,
+    "scaleType": "linear",
+    "spaceRef": null,
+    "syncModels": [],
+    "use": "indicator",
+    "which": "총인구",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "size_label": {
+    "_important": false,
+    "allow": {
+     "names": [
+      "_default"
+     ],
+     "scales": [
+      "ordinal",
+      "linear",
+      "log",
+      "genericLog",
+      "pow"
+     ]
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "extent": [
+     0,
+     0.18
+    ],
+    "fixBaseline": 0,
+    "scaleType": "ordinal",
+    "spaceRef": null,
+    "syncModels": [],
+    "use": "constant",
+    "which": "_default",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "space": [
+    "entities",
+    "time"
+   ],
+   "superHighlight": []
+  },
+  "marker_colorlegend": {
+   "allowSelectMultiple": true,
+   "highlight": [],
+   "hook_geoshape": {
+    "data": "data",
+    "syncModels": [],
+    "use": "property",
+    "which": "shape_lores_svg"
+   },
+   "hook_rank": {
+    "data": "data",
+    "syncModels": [],
+    "use": "property",
+    "which": "rank"
+   },
+   "label": {
+    "data": "data",
+    "syncModels": [],
+    "use": "property",
+    "which": "name"
+   },
+   "limit": 1000,
+   "opacityHighlightDim": 0.1,
+   "opacityRegular": 1,
+   "opacitySelectDim": 0.3,
+   "select": [],
+   "space": [
+    "entities_colorlegend"
+   ],
+   "superHighlight": []
+  },
+  "time": {
+   "autoconfig": {
+    "type": "time"
+   },
+   "delay": 393,
+   "delayThresholdX2": 90,
+   "delayThresholdX4": 45,
+   "dim": "time",
+   "end": "2021",
+   "endBeforeForecast": "2021",
+   "endOrigin": null,
+   "endSelected": "2021",
+   "format": {
+    "data": null,
+    "ui": null
+   },
+   "immediatePlay": true,
+   "loop": false,
+   "offset": 0,
+   "pauseBeforeForecast": true,
+   "playable": true,
+   "playing": false,
+   "record": false,
+   "round": "round",
+   "showForecast": true,
+   "start": "1992",
+   "startOrigin": null,
+   "startSelected": "1992",
+   "step": 1,
+   "unit": "year",
+   "value": "2021"
+  }
+ },
+ "ui": {
+  "adaptMinMaxZoom": false,
+  "buttons": [
+   "colors",
+   "find",
+   "zoom",
+   "trails",
+   "lock",
+   "moreoptions",
+   "presentation",
+   "sidebarcollapse",
+   "fullscreen"
+  ],
+  "chart": {
+   "decorations": {
+    "enabled": true,
+    "xAxisGroups": null
+   },
+   "labels": {
+    "dragging": true,
+    "removeLabelBox": false
+   },
+   "lockNonSelected": 0,
+   "margin": {
+    "left": 99.81,
+    "top": 0
+   },
+   "showForecastOverlay": false,
+   "superhighlightOnMinimapHover": true,
+   "trails": true,
+   "whenHovering": {
+    "higlightValueX": true,
+    "higlightValueY": true,
+    "showProjectionLineX": true,
+    "showProjectionLineY": true
+   }
+  },
+  "cursorMode": "arrow",
+  "datawarning": {
+   "doubtDomain": [],
+   "doubtRange": []
+  },
+  "dialogs": {
+   "dialog": {
+    "find": {
+     "enableSelectShowSwitch": false
+    }
+   },
+   "moreoptions": [
+    "opacity",
+    "speed",
+    "axes",
+    "size",
+    "colors",
+    "label",
+    "zoom",
+    "presentation",
+    "technical",
+    "about"
+   ],
+   "popup": [
+    "colors",
+    "find",
+    "size",
+    "zoom",
+    "moreoptions"
+   ],
+   "sidebar": [
+    "colors",
+    "find",
+    "size",
+    "zoom"
+   ]
+  },
+  "panWithArrow": false,
+  "presentation": false,
+  "show_ticks": true,
+  "sidebarCollapse": false,
+  "splash": false,
+  "zoomOnScrolling": false
+ },
+ "chartType": "BubbleChart"
+};
